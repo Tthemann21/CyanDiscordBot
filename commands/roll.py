f07096d415b62fa7f72b41b6d9e4823611b6041f
Tthemann <morganlpearson22@gmail.com>
2025-11-27 00:59:45 -0500

    added basic currenecy and betting added db system with abilitys to handle user_id and currency
    Co-authored-by: Zeptofine <zeptofine@users.noreply.github.com>

--- commands/roll.py
+++ commands/roll.py
@@ -1,11 +1,55 @@
 import random
 
 from discord.ext import commands
-
+from db_db import get_or_create_user, set_balance
 
 class Roll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name = 'Betroll', help = 'Roll a dice and bet on a lucky number! Usage: !betroll <bet amount> <lucky number> [range of dice, default 20]')
+    async def betroll(self, ctx: commands.Context, bet: int, lucky_number: int, range: int = 20):
+        user_id = ctx.author.id
+
+        if bet<=0:
+            return await ctx.send("Bet amount must be greater than zero.")
+        if lucky_number < 1 or lucky_number > range:
+            return await ctx.send(f"Your lucky number must be between 1 and {range}.")
+        current_balance = get_or_create_user(user_id) 
+        if bet > current_balance:
+            return await ctx.send(f"You do not have enough balance to place that bet. Your current balance is {current_balance}.")
+
+        await ctx.send(f"Rolling a D{range}...")
+        await ctx.send(f"You bet {bet} coins on lucky number {lucky_number}.")
+        
+        result = random.randint(1, range)
+        
+        if result == lucky_number:
+            Payoutfactor = range - 1
+            Payout = bet * Payoutfactor
+            new_balance = current_balance+Payout
+            change_success = set_balance(user_id, new_balance)
+
+            if change_success:
+                await ctx.send(f"JACKPOT! You rolled a {result}! and won {Payout} coins! Your new balance is {new_balance}.")
+            else:
+                await ctx.send("Error updating balance.")
+        else:
+            new_balance = current_balance - bet
+            change_success = set_balance(user_id, new_balance)
+            if change_success:
+                await ctx.send(f"You rolled a {result}. Sorry, you lost {bet} coins. Your new balance is {new_balance}.")
+            else:
+                await ctx.send("Error updating balance.")
+
+    @betroll.error
+    async def betroll_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please specify the number you wish to chose ex !roll 6.")
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send("Please enter a valid integer for the number.")
+        else:
+            await ctx.send("An error occurred while processing the command.")
 
     @commands.command()
     async def roll(self, ctx: commands.Context, lucky_number: int, range: int = 20):
---
+++ db_db.py
@@ -0,0 +1,88 @@
+import sqlite3
+from typing import Optional
+
+DB_FILE = "db_discordbot.db"
+startingBalance = 500
+
+
+def database_setup():
+    try:
+        with sqlite3.connect(DB_FILE) as con:
+            cursor = con.cursor()
+
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS player_currency (
+                user_id INTEGER PRIMARY KEY, 
+                balance INTEGER
+                        )
+                """)
+            con.commit()
+            print("Database setup completed.")
+    except sqlite3.Error as e:
+        print(f"ERROR: Could not set up database: {e}")
+
+
+def get_or_create_user(user_id: int):
+    try:
+        with sqlite3.connect(DB_FILE) as con:
+            cursor = con.cursor()
+
+            cursor.execute('SELECT balance FROM player_currency WHERE user_id = ?', (user_id,))
+            result: Optional[tuple] = cursor.fetchone()
+
+            if result is None:
+                #may have to be refactored into execute many depending on popularity
+                # user does not exist insert new record in table 
+                cursor.execute(
+                    'INSERT INTO player_currency (user_id, balance) VALUES (?, ?)',
+                    (user_id, startingBalance)
+                )
+                con.commit()
+                return startingBalance
+            else:
+                #User exists return current balance
+                return result[0]
+    except sqlite3.Error as e:
+        print(f"Database error during read/create: {e}")
+        return 0
+    
+
+# bal = bal + change
+def add_balance(user_id: int, amount_change: int):
+    try:    
+        with sqlite3.connect(DB_FILE) as con:
+            cursor = con.cursor()
+
+            cursor.execute('SELECT balance FROM player_currency WHERE user_id = ?', (user_id,))
+            current_balance = cursor.fetchone()[0]
+
+            if current_balance + amount_change < 0:
+                return False
+            
+            cursor.execute(
+                'UPDATE player_currency SET balance = balance + ? WHERE user_id = ?',
+                (amount_change,user_id)
+            )
+            con.commit()
+            return True
+    except sqlite3.Error as e:
+        print(f"Database error during update: {e}")
+        return False
+
+# bal = new_balance
+def set_balance(user_id: int, new_balance: int):
+    if new_balance < 0:
+        return False
+    try:    
+        with sqlite3.connect(DB_FILE) as con:
+            cursor = con.cursor()
+
+            cursor.execute(
+                'UPDATE player_currency SET balance = ? WHERE user_id = ?',
+                (new_balance,user_id)
+            )
+            con.commit()
+            return True
+    except sqlite3.Error as e:
+        print(f"Database error during update: {e}")
+        return False
